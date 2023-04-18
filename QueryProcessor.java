a6abfdbaf12a0edd857e69085a23e5849e08d3a2
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2023-04-18 16:33:08 +0200

    Query Processor

---
+++ QueryProcessor.java
@@ -0,0 +1,45 @@
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import java.util.ArrayList;
+
+
+import java.io.IOException;
+import java.io.StringReader;
+
+class query {
+    public static void QueryProcessor(String prompt) {
+        ArrayList<String> words = new ArrayList<String>();
+        Analyzer analyzer = new EnglishAnalyzer();
+        TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(prompt));
+
+        TokenStream porterFilter = new PorterStemFilter(tokenStream);
+        CharTermAttribute termAtt = porterFilter.addAttribute(CharTermAttribute.class);
+
+        try {
+            porterFilter.reset();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        while (true) {
+            try {
+                if (!porterFilter.incrementToken()) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            String term = termAtt.toString();
+            words.add(term);
+        }
+
+        System.out.println(words);
+    }
+
+    public static void main(String[] args) {
+        QueryProcessor("This is a new play field");
+        QueryProcessor("Hello world from the computer department with a new computing power");
+    }
+}
+
