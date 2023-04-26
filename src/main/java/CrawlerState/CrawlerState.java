7776f3a1c43447caf5948e1c6c1a8d1dab33c2f6
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2023-04-26 18:33:38 +0200

    Crawler V1.2 Finished

---
+++ src/main/java/CrawlerState/CrawlerState.java
@@ -0,0 +1,68 @@
+package CrawlerState;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class CrawlerState implements Serializable {
+
+
+    private static final long serialVersionUID = 1L;
+    private final AtomicInteger current_Turn;
+    private final int flag;
+    private final ConcurrentLinkedQueue<String>[] BFS;
+    private final int numThreads;
+    private final AtomicInteger count ;
+    private final AtomicInteger level ;
+    private final Set<String> links;
+    private final Set<String> Doc;
+    public CrawlerState(ConcurrentLinkedQueue<String>[] BFS , int numThreads, AtomicInteger current_Turn , int flag,AtomicInteger c , AtomicInteger l  , Set<String> li , Set<String> doc) {
+        this.BFS = BFS;
+        this.numThreads = numThreads;
+        this.current_Turn = current_Turn;
+        this.flag =flag ;
+        count = c;
+        level = l;
+        links = li;
+        Doc = doc;
+    }
+
+
+    public ConcurrentLinkedQueue<String>[] getBFS() {
+        return BFS;
+    }
+
+    public int getNumThreads() {
+        return numThreads;
+    }
+
+    public AtomicInteger getCurrent_Turn(){
+        return current_Turn;
+    }
+
+    public int getFlag()
+    {
+        return flag;
+    }
+
+
+    public AtomicInteger getCount()
+    {
+        return count;
+    }
+    public AtomicInteger getLevel()
+    {
+        return level;
+    }
+
+    public Set<String> getLinks()
+    {
+        return links;
+    }
+
+    public Set<String> getDoc()
+    {
+        return Doc;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,39 @@
 import Crawler.Crawler;
 import Crawler.Globals;
+import CrawlerState.CrawlerState;
 import MongoDB.MongoInterface;
 import org.bson.Document;
 import org.jsoup.Jsoup;
-import org.jsoup.select.Elements;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+
+
 public class Main {
+
     public static void main(String[] args) throws InterruptedException {
 
+        CrawlerState state = null;
+        String filePath = "crawler_state.ser";
+
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            try (FileInputStream fileIn = new FileInputStream(file);
+                 ObjectInputStream in = new ObjectInputStream(fileIn)) {
+                state = (CrawlerState) in.readObject();
+                System.out.println("Crawler state loaded from file: " + filePath);
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+                // Handle the exception appropriately
+            }
+        }
 
         MongoInterface.Initialize();
         MongoInterface.deleteAllDocuments("URlS_DOCS");
@@ -27,41 +44,85 @@ public class Main {
         Set<String> Doc_Spec_txt=ConcurrentHashMap.newKeySet();
         Set<String>links= ConcurrentHashMap.newKeySet();
         ConcurrentLinkedQueue<String>[] BFS = new ConcurrentLinkedQueue[50];
-        for (int i = 0; i < 50; i++) {
-            BFS[i] = new ConcurrentLinkedQueue<>();
-        }
-        try {
-            File myObj = new File("src/main/java/Crawler/seed.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNext()) {
-                String link = myReader.next();
-                seed.add(link);
+
+
+
+        if (state==null || state.getFlag() == 1) {
+            for (int i = 0; i < 50; i++) {
+                BFS[i] = new ConcurrentLinkedQueue<>();
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            try {
+                File myObj = new File("src/main/java/Crawler/seed.txt");
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNext()) {
+                    String link = myReader.next();
+                    seed.add(link);
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            //System.out.println(seed);
+            //Crawling
+            //crawling threads
+            System.out.println("Please, enter the desired number of threads");
+
+            Scanner input = new Scanner(System.in);
+            Globals.numThreads = input.nextInt();
+
+
+            for (int i = 0; i < 21; i++) {
+                BFS[0].add(seed.peek());
+                seed.remove();
+            }
+            Globals.levelNum.set(BFS[0].size());
+
         }
-          //System.out.println(seed);
-        //Crawling
-        //crawling threads
-        System.out.println("Please, enter the desired number of threads");
 
-        Scanner input = new Scanner(System.in);
-        Globals.numThreads = input.nextInt();
-
-        Thread[] threads = new Thread[ Globals.numThreads];
-
-        for(int i=0;i<21;i++)
+        else
         {
-            BFS[0].add(seed.peek());
-            seed.remove();
+            BFS = state.getBFS();
+            links = state.getLinks();
+            Globals.numThreads = state.getNumThreads();
+            Globals.turn = (state.getCurrent_Turn());
+            Globals.count = state.getCount();
+            Globals.levelNum.set(BFS[Globals.turn.get()].size());
+            Doc_Spec_txt = state.getDoc();
+
         }
-        Globals.levelNum.set(BFS[0].size());
+
+        Thread[] threads = new Thread[Globals.numThreads];
+
+
+
+
+
         for (int i = 0; i < Globals.numThreads; i++) {
             threads[i] = new Thread(new Crawler(BFS,links,Doc_Spec_txt));
             threads[i].setName(Integer.toString(i));
             threads[i].start();
         }
+
+
+
+        ConcurrentLinkedQueue<String>[] finalBFS = BFS;
+        Set<String> finalLinks = links;
+        Set<String> finalDoc_Spec_txt = Doc_Spec_txt;
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            int flag = 0;
+            if (Globals.count.get() <= 0) {
+                flag = 1;
+            }
+            CrawlerState Runstate = new CrawlerState(finalBFS, Globals.numThreads, Globals.turn, flag, Globals.count, Globals.levelNum, finalLinks, finalDoc_Spec_txt);
+            try (FileOutputStream fileOut = new FileOutputStream("crawler_state.ser");
+                 ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
+                out.writeObject(Runstate);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+
 
         for (int i = 0; i < Globals.numThreads; i++) {
             threads[i].join();
@@ -69,26 +130,31 @@ public class Main {
         }
 
         System.out.println("Crawler Finished got : "+ links.size()+" websites inserting them to ExcessDB");
-        MongoInterface.Initialize();
 
-        links.forEach(link -> {
-            try {
+        if (Globals.count.get() <= 0 ) {
+            MongoInterface.Initialize();
 
-                org.jsoup.nodes.Document doc = Jsoup.connect(link).get();
-                Document document = new Document("URL", link)
-                        .append("DOC", doc.toString());
+            links.forEach(link -> {
+                try {
 
-                // Insert the text into the MongoDB collection
-                MongoInterface.insertDocument("ExcessDB","URlS_DOCS",document);
+                    org.jsoup.nodes.Document doc = Jsoup.connect(link).get();
+                    Document document = new Document("URL", link)
+                            .append("DOC", doc.toString());
 
-            } catch (IOException e) {
-                System.out.println("Error fetching URL: " + e.getMessage());
-            }
-        });
-        MongoInterface.terminate();
+                    // Insert the text into the MongoDB collection
+                    MongoInterface.insertDocument("ExcessDB", "URlS_DOCS", document);
+
+                } catch (IOException e) {
+                    System.out.println("Error fetching URL: " + e.getMessage());
+                }
+            });
+            MongoInterface.terminate();
+
+        }
 
         //User Query Processing
 //        query.QueryProcessor("This is a new play field");
 //        query.QueryProcessor("Hello world from the computer department with a new computing power");
     }
+
 }
