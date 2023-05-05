a16a0c6576ab92984c68fca3eec14a807be4620b
Aly-Abdel-Motaleb <94588581+Aly-Abdel-Motaleb@users.noreply.github.com>
2023-05-05 21:04:24 +0300

    Finishing the ranking

--- src/main/java/PageRanker/RankerMain.java
+++ src/main/java/PageRanker/RankerMain.java
@@ -5,19 +5,31 @@ import org.bson.Document;
 
 import java.util.*;
 
+import static QueryProcessor.queryP.QueryProcessor;
+
 public class RankerMain {
 
 
     public static void main(String[] args){
 
-        String searchQuery[]={"don"};
+        String prompt = "Hello let's play football";
+
+        // placeholder for the query processor
+
+        Scanner s= new Scanner(System.in);
+
+        prompt = s.next();
+
+        ArrayList<String> values = QueryProcessor(prompt);
+
+        String searchQuery[]= values.toArray(new String[values.size()]);
+
         Map<String, Integer> Snippets = new HashMap<>();
 
         Map<String, linkAttr> toDisplayTmp = new HashMap<>();
         MongoInterface.Initialize();
 
         for (int i = 0; i < searchQuery.length; i++) {
-
 
             final double numOfdocs = 6000;
 
@@ -29,8 +41,9 @@ public class RankerMain {
                     String url = websiteDoc.getString("URL");
                     String title = websiteDoc.getString("Title");
                     Double tf = Double.parseDouble(websiteDoc.getString("TF"));
-                    Double pri = Double.parseDouble(websiteDoc.getString("Pri"));
+                    Double pri = Math.log(Double.parseDouble(websiteDoc.getString("Pri")))* 0.1;
                     Double idf = Math.log(numOfdocs / df);
+
                     if (!toDisplayTmp.containsKey(url)) {
                         linkAttr tmp = new linkAttr();
                         tmp.title = title;
@@ -42,11 +55,12 @@ public class RankerMain {
                         tmp.pri += (tf * idf + pri);
                         toDisplayTmp.put(url, tmp);
                     }
-                    System.out.println("Website URL: " + url);
+                    // System.out.println("Website URL: " + url);
                 }
             }
         }
         MongoInterface.terminate();
+        // use the popularity in the calculation
 
         Comparator<String> valueComparator = new Comparator<String>() {
             @Override
@@ -58,5 +72,9 @@ public class RankerMain {
         };
         Map<String, linkAttr> toDisplay = new TreeMap<>(valueComparator);
         toDisplay.putAll(toDisplayTmp);
+
+        toDisplay.forEach((key, value) -> {
+            System.out.println("Website: " + key + ", Priority: " + value.pri);
+        });
     }
     }
--- src/main/java/PageRanking/PageRankingAlgo.java
+++ src/main/java/PageRanking/PageRankingAlgo.java
@@ -1,7 +1,29 @@
 package PageRanking;
 
+import MongoDB.MongoInterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PageRankingAlgo {
 
+    public static void main(String args[])
+    {
+        MongoInterface.Initialize();
 
+        HashMap<String,Double> hash = new HashMap<>();
+
+        String [] webs = {"www.google.com","www.facebook.com"};
+
+        hash = MongoInterface.getWebsitePopularity(webs);
+
+        for (HashMap.Entry<String, Double> entry : hash.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+
+        MongoInterface.terminate();
+
+    }
 
 }
